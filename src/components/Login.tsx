3b6414b7d34d471eaa51c709733b4d060b139598
KimbohLovette <kimbohlovette@gmail.com>
2023-06-07 18:04:50 +0100

    Added login page to admin dashboard

---
+++ src/components/Login.tsx
@@ -0,0 +1,87 @@
+import Image from 'next/image';
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+	title: 'Login to your account',
+	description: 'Login to monitor app statistics and user activities',
+};
+const Login = () => {
+	const logoImage: string = require('src/assets/whoget-primary.png');
+	return (
+		<div>
+			<div className="flex min-h-full flex-1 flex-col justify-center items-center px-6 mt-16 py-2 lg:px-8">
+				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+					<Image
+						src={logoImage}
+						className="mx-auto max-h-20 w-auto"
+						alt="Whoget logo"
+					/>
+					<h2 className="text-center text-2xl font-medium leading-9 tracking-tight text-gray-900">
+						Sign in to your account
+					</h2>
+				</div>
+
+				<div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+					<div className="space-y-6">
+						<div>
+							<label
+								htmlFor="email"
+								className="block text-sm font-medium leading-6 text-gray-900"
+							>
+								Email address
+							</label>
+							<div className="mt-2">
+								<input
+									id="email"
+									name="email"
+									type="email"
+									autoComplete="email"
+									required
+									className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div>
+							<div className="flex items-center justify-between">
+								<label
+									htmlFor="password"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									Password
+								</label>
+								<div className="text-sm">
+									<a
+										href="#"
+										className="font-semibold text-primary-600 hover:text-primary-500"
+									>
+										Forgot password?
+									</a>
+								</div>
+							</div>
+							<div className="mt-2">
+								<input
+									id="password"
+									name="password"
+									type="password"
+									autoComplete="current-password"
+									required
+									className="block w-full rounded-md border-0 px-2 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div>
+							<button className="flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+								Sign in
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
