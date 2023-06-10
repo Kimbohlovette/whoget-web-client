b059abcd6d3cf928ef649f29c6875edab8399455
KimbohLovette <kimbohlovette@gmail.com>
2023-06-10 05:24:42 +0100

    If user navigates to login while authenticated redirect back to home

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,19 @@
 import Login from '@/components/Login';
-import React from 'react';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { updateAuthStatus } from '@/store/slices/userSlice';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 
 const Authenticate = () => {
+	const router = useRouter();
+	const isAuthenticated = useAppSelector(
+		(state) => state.user.isAuthenticated
+	);
+	useEffect(() => {
+		if (isAuthenticated) {
+			router.push('/');
+		}
+	}, [isAuthenticated]);
 	return (
 		<>
 			<Login />
